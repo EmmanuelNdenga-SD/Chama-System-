ac946730a03df9db8a319d1ec6ba8d1589402e94
emmanue-ndenga <emmanuel.ndenga@student.moringaschool.com>
2025-07-01 06:20:05 +0300

    Fix Render deployment: move wsgi.py to root

--- server/wsgi.py
+++
@@ -1,4 +0,0 @@
-# wsgi.py (in your root directory)
-from server.app import create_app
-
-app = create_app()
--- wsgi.py
+++ wsgi.py
@@ -1,4 +1,3 @@
-# wsgi.py (in your root directory)
 from server.app import create_app
 
 app = create_app()
